a94ef52a15df77eedb0569c5f79f2559106c8e56
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2025-04-10 19:07:57 -0300

    feat: iniciando o projeto

---
+++ src/main/java/br/com/lfmelo/TcFastFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.lfmelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcFastFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcFastFoodApplication.class, args);
+	}
+
+}
