6c8cd8b412a1593bd4fc324b06e9b22dcb04ff49
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2025-04-15 22:08:46 -0300

    feat: criando domains brutas

--- src/main/java/br/com/lfmelo/domains/clients/Client.java
+++ src/main/java/br/com/lfmelo/domains/clients/Client.java
@@ -2,4 +2,76 @@ package br.com.lfmelo.domains.clients;
 
 public class Client {
 
+    private Long id;
+
+    private String cpf;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String phone;
+
+    public Client() {
+
+    }
+
+    public Client(Long id, String cpf, String name, String email, String password, String phone) {
+        this.id = id;
+        this.cpf = cpf;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
